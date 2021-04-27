bb83abd3e3d07a791e8268504ea7d1a69b6326b4
cristianS97 <cristiansaez97@gmail.com>
2021-04-27 18:27:17 -0400

    Inicio clase conexiones bbdd

---
+++ conexiones.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Autor: Cristian Sáez Mardones
+# Fecha: 18-04-2021
+# Versión: 1.0.0
+# Objetivo: Crear un juego de batallas pokemon
+
+# Importación de archivo
+    # No hay
+# Importación de bibliotecas
+    # Si hay
+# Importación de funciones
+    # No hay
+
+### Bibliotecas ###
+# Biblioteca para manejo de bbdd
+import sqlite3
+
+class Conexion:
+    def __init__(self) -> None:
+        pass
+    
+    def cargar(self, data) -> None:
+        pass
+
+    def conectar(self) -> sqlite3.Connection:
+        pass
+
+    def consultar_pokemon(self, pokemon) -> sqlite3.Connection:
+        pass
